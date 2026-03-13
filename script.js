c71f42284c1fa2a2336d6a68c5b2f771653c72cb
shakhriyor-dev <shakh2616@gmail.com>
2026-03-13 13:46:03 +0500

    ultimate-animated-login-ui

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-// cursor glow
-
+// Cursor Glow
 const cursor = document.querySelector(".cursor");
 
 document.addEventListener("mousemove", (e) => {
@@ -7,14 +6,22 @@ document.addEventListener("mousemove", (e) => {
   cursor.style.top = e.clientY + "px";
 });
 
-// particles
+// 3D Card Parallax
+const card = document.querySelector(".card");
 
+document.addEventListener("mousemove", (e) => {
+  let x = (window.innerWidth / 2 - e.pageX) / 20;
+  let y = (window.innerHeight / 2 - e.pageY) / 20;
+  card.style.transform = `rotateY(${x}deg) rotateX(${y}deg)`;
+});
+
+// Particles.js Configuration
 particlesJS("particles-js", {
   particles: {
-    number: { value: 80 },
+    number: { value: 90 },
     color: { value: "#00ffff" },
     shape: { type: "circle" },
-    opacity: { value: 0.5 },
+    opacity: { value: 0.6 },
     size: { value: 3 },
     move: { enable: true, speed: 2 },
   },
